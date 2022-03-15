e03de58ea5d38d4f915dde455f3b5c15733fd7f0
Kiara Richardson <kiararich722@icloud.com>
2022-03-15 07:46:57 -0400

    contact page updates

--- JS/index.js
+++ JS/index.js
@@ -3,4 +3,4 @@ $(function() {
 
 });
 
-//add a dismiss or "hide" event to the modal if DOB === 21 +
+//add an age calulator based on DOB from user input, and dismiss or "hide" the modal if age is >= 21. Sustain modal visibility if < 21.
