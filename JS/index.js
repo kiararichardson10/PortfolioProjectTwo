996826ded4a17e72902c00522c06328769a1a191
Kiara Richardson <kiararich722@icloud.com>
2022-03-18 19:02:51 -0400

    site updates

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,20 @@
 $(function() {
     $("#ageCheckModal").modal("show");
+        
+   /* an age calulator based on DOB from user input. Will dismiss or "hide" the modal if age is >= 21. Sustain modal visibility if < 21.
+    
+   $("#confirmbtn").click(function(dob) {
+        const ageInTime = (Date.now() - dob.getTime());
+        const ageDate = new Date(ageInTime);
+        const currentAge = Math.abs(ageDate.getUTCFullYear() -1970);
 
+        if(currentAge >= 21) {
+            $("ageCheckModal").modal("hide");
+        } else if (currentAge < 21) {
+            $("ageCheckModal").modal("show");
+        };
+    });
+    */
 });
 
-//add an age calulator based on DOB from user input, and dismiss or "hide" the modal if age is >= 21. Sustain modal visibility if < 21.
+
